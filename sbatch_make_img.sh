517d47c6234e88af5a2ad7a84b92aad54cf7d334
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-01-17 17:10:24 +0100

    changed img out

--- sbatch_make_img.sh
+++ sbatch_make_img.sh
@@ -10,16 +10,16 @@ CROP=$1 #crop
 mkdir -p img/${CROP}
 FOLDER=$( pwd )
 IMG=~/singularity/python/python3.7_2.0.sif
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/historical out=img/${CROP}/historical &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_GFDL-CM3_45 out=img/${CROP}/2_GFDL-CM3_45 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_GFDL-CM3_85 out=img/${CROP}/2_GFDL-CM3_85 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_GISS-E2-R_45 out=img/${CROP}/2_GISS-E2-R_45 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_GISS-E2-R_85 out=img/${CROP}/2_GISS-E2-R_85 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_HadGEM2-ES_45 out=img/${CROP}/2_HadGEM2-ES_45 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_HadGEM2-ES_85 out=img/${CROP}/2_HadGEM2-ES_85 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_MIROC5_45 out=img/${CROP}/2_MIROC5_45 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_MIROC5_85 out=img/${CROP}/2_MIROC5_85 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_MPI-ESM-MR_45 out=img/${CROP}/2_MPI-ESM-MR_45 &
-singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_MPI-ESM-MR_85 out=img/${CROP}/2_MPI-ESM-MR_85 &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/historical out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_GFDL-CM3_45 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_GFDL-CM3_85 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_GISS-E2-R_45 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_GISS-E2-R_85 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_HadGEM2-ES_45 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_HadGEM2-ES_85 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_MIROC5_45 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_MIROC5_85 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_MPI-ESM-MR_45 out=img/${CROP} &
+singularity run -B $FOLDER:$FOLDER $IMG python gen_img/create_image_from_ascii.py source=crops/${CROP}/2_MPI-ESM-MR_85 out=img/${CROP} &
 
 wait
